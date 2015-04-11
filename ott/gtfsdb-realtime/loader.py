5de1d1854586d57d65b1cbb60e9423b790caaaf9
Frank Purcell <fxpurcell@gmail.com>
2015-04-11 11:53:35 -0700

    running loader.py

--- ott/gtfsdb-realtime/loader.py
+++ ott/gtfsdb-realtime/loader.py
@@ -2,7 +2,9 @@ from google.transit import gtfs_realtime_pb2
 import urllib
 
 feed = gtfs_realtime_pb2.FeedMessage()
-response = urllib.urlopen('URL OF YOUR GTFS-REALTIME SOURCE GOES HERE')
+url = 'http://trimet.org/transweb/ws/V1/FeedSpecAlerts/appId/3819A6A38C72223198B560DF0/includeFuture/true'
+url = 'http://trimet.org/transweb/ws/V1/TripUpdate/appId/3819A6A38C72223198B560DF0/includeFuture/true'
+response = urllib.urlopen(url)
 feed.ParseFromString(response.read())
 for entity in feed.entity:
   if entity.HasField('trip_update'):
