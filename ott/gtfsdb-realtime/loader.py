5b82825e2782a2ee0be19c51cddb033ab17b3e36
Frank Purcell <fxpurcell@gmail.com>
2015-04-11 12:58:01 -0700

    fix up .ignore and add vehicles to loader

--- ott/gtfsdb-realtime/loader.py
+++ ott/gtfsdb-realtime/loader.py
@@ -4,8 +4,10 @@ import urllib
 feed = gtfs_realtime_pb2.FeedMessage()
 url = 'http://trimet.org/transweb/ws/V1/FeedSpecAlerts/appId/3819A6A38C72223198B560DF0/includeFuture/true'
 url = 'http://trimet.org/transweb/ws/V1/TripUpdate/appId/3819A6A38C72223198B560DF0/includeFuture/true'
+url = 'http://developer.trimet.org/ws/gtfs/VehiclePositions/appId/3819A6A38C72223198B560DF0'
 response = urllib.urlopen(url)
 feed.ParseFromString(response.read())
 for entity in feed.entity:
+  print entity
   if entity.HasField('trip_update'):
     print entity.trip_update
