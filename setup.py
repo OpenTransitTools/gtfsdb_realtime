602cdde9dbe90d05803dcf3ce59cc59dca685489
Frank Purcell <fxpurcell@gmail.com>
2015-04-23 18:00:23 -0700

    fix up setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 import os
-import sys
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-oracle_extras = ['cx_oracle>=5.1']
-postgresql_extras = ['psycopg2>=2.4.2']
-dev_extras = postgresql_extras
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-extras_require = dict(
-    dev=dev_extras,
-    oracle=oracle_extras,
-    postgresql=postgresql_extras,
-)
-
-install_requires = [
+requires = [
     'ott.utils',
     'argparse',
     'simplejson',
@@ -24,18 +16,30 @@ install_requires = [
     'gtfs-realtime-bindings',
 ]
 
+
+dev_extras = []
+oracle_extras = ['cx_oracle>=5.1']
+postgresql_extras = ['psycopg2>=2.4.2']
+
+extras_require = dict(
+    dev=dev_extras,
+    postgresql=postgresql_extras,
+)
+
 if sys.version_info[:2] <= (2, 6):
-    install_requires.append('argparse>=1.2.1')
+    requires.append('argparse>=1.2.1')
     extras_require['dev'].append('unittest2')
 
 setup(
     name='ott.gtfsdb_realtime',
     version='0.1.0',
     description='GTFS Real-time Database',
-    long_description=open('README').read(),
+    long_description=README + '\n\n' + CHANGES,
     keywords='GTFS,GTFS-realtime,GTFSRT',
     url='http://opentransittools.com',
     license="Mozilla-derived (http://opentransittools.com)",
+    author="Open Transit Tools",
+    author_email="info@opentransittools.org",
     classifiers=(
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -44,15 +48,13 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
     ),
-    author='Frank Purcell',
-    author_email='purcellf@trimet.org',
     dependency_links=[
         'git+https://github.com/OpenTransitTools/utils.git#egg=ott.utils-0.1.0',
     ],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
+    install_requires=requires,
     extras_require=extras_require,
     entry_points="""\
         [console_scripts]
