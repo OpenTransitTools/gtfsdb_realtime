cb282a85bec7684609f94efb97b25199565732ab
Frank Purcell <fxpurcell@gmail.com>
2018-04-26 23:51:29 -0700

    minor rename + docs

--- README.md
+++ README.md
@@ -1,63 +1,34 @@
+
 GTFSDB Realtime
-+++++++++++++++
+===
+
 
 Supported Databases
-===================
+---
 
 - PostgreSQL (PostGIS for Geo tables) - preferred
-- SQLite - tested
+- SQLite - not tested yet
 - Oracle - not tested yet
 - MySQL  - not tested yet
 
+
 GTFS REALTIME (General Transit Feed Specification) Database
-===========================================================
+--
 
 Python code that will load GTFS-realtime data into a relational database, and SQLAlchemy ORM bindings to the GTFS tables in the gtfsdb.
 See https://developers.google.com/transit/gtfs-realtime/
 
 
 Install and use via the gtfsdb source tree:
-==========================================
+---
 
-0. Install Python 2.7, easy_install and buildout on your system...
+1. Install Python 2.7, easy_install and buildout on your system...
 1. git clone https://github.com/OpenTransitTools/gtfsdb_realtime.git
-2. cd gtfsdb_realtime
-3. buildout
-   NOTE: if you're using postgres, do a 'buildout install prod pg'
-4. bin/loader --database_url <db url>  <gtfs file | url>
-   examples:
-   - bin/loader --database_url sqlite:///gtfsrt.db --url http://developer.trimet.org/ws/gtfs/VehiclePositions/appId/<your app id>
-   - bin/loader --database_url sqlite:///gtfsrt.db --url http://developer.trimet.org/ws/gtfs/TripUpdate/appId/<your app id>
-   - bin/loader --database_url sqlite:///gtfsrt.db --url http://developer.trimet.org/ws/gtfs/FeedSpecAlerts/includeFuture/true/appId/<your app id>
-   - bin/loader --database_url postgresql://postgres@localhost:5432/<db name> --schema gtfsdb_realtime --geo --url http://developer.trimet.org/ws/gtfs/VehiclePositions/appId/<your app id>
-
-The best way to get gtfsdb_realtime up and running is via the python 'buildout' and 'easy_install' tools.
-Highly recommended to first install easy_install (setup tools) and buildout (e.g., easy_install zc.buildout)
-before doing anything else.
-
-Postgres users, gtfsdb_realtime requires the psycopg2 database driver. If you are on linux / mac, buildout will install
-the necessary dependencies (or re-use whatever you have in your system site-lib). If you are on windows, you most
-likely have to find and install a pre-compiled version (see below).
+1. cd gtfsdb_realtime
+1. buildout (note: if you're using postgres, do a 'buildout install prod pg')
 
 
-Install Steps (on Windows):
----------------------------
-    0. Have a db - docs and examples assume Postgres/PostGIS installed
-       http://www.postgresql.org/download/windows
-       http://postgis.refractions.net/download/windows/
-
-    1. Python2.7 - http://www.python.org/download/releases/2.7.6/ (python-2.7.6.msi)
-       NOTE: see this for setting env variables correctly: http://blog.sadphaeton.com/2009/01/20/python-development-windows-part-1installing-python.html
-
-    2a. Install Setup Tools (easy_install) https://pypi.python.org/pypi/setuptools#windows-8-powershell
-    2b. easy_install zc.buildout
-
-    3. Install Psygopg2 (from binary):  http://www.stickpeople.com/projects/python/win-psycopg/
-
-    4. Check out gtfsdb from trunk with Git - see: git clone https://github.com/OpenTransitTools/gtfsdb_realtime.git
-
-    5. cd top level of gtfsdb_realtime tree
-    
-    6. buildout install prod
-
-    7. bin/loader --database_url <db url>  --url <url to GTFS-realtime data>
+Run TriMet example:
+---
+1. get a TriMet api key: http://developer.trimet.org/appid/registration/
+1. bin/gtfsdb-rt-loader -c -a trimet -d postgresql+psycopg2://ott@127.0.0.1:5432/ott --api_key <trimet api key>
--- ott/gtfsdb_realtime/loader.py
+++ ott/gtfsdb_realtime/loader.py
@@ -9,29 +9,34 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__file__)
 
 
-def load_agency_gtfsrt_data(session, agency_id, alerts_url, trips_url, vehicles_url):
+def load_multiple_agency_feeds(session, agency_id, alerts_url=None, trips_url=None, vehicles_url=None):
+    """
+    This is a main entry for loading one or more GTFS-RT feeds ...
+    """
     ret_val = True
 
     if alerts_url:
-        r = process(session, agency_id, alerts_url)
+        r = load_gtfsrt_feed(session, agency_id, alerts_url)
         if not r:
             ret_val = False
 
     if trips_url:
-        r = process(session, agency_id, trips_url)
+        r = load_gtfsrt_feed(session, agency_id, trips_url)
         if not r:
             ret_val = False
 
     if vehicles_url:
-        r = process(session, agency_id, vehicles_url)
+        r = load_gtfsrt_feed(session, agency_id, vehicles_url)
         if not r:
             ret_val = False
 
     return ret_val
 
 
-def process(session, agency_id, feed_url, clear_tables_first=True):
-    """ will grab a GTFS-RT feed, and store it in a database
+def load_gtfsrt_feed(session, agency_id, feed_url, clear_tables_first=True):
+    """
+    this is a main entry for loading a single GTFS-RT feed
+    the logic here will grab a GTFS-RT feed, and store it in a database
     """
     feed = grab_feed(feed_url)
     feed_type = Base.get_feed_type(feed)
@@ -40,23 +45,6 @@ def process(session, agency_id, feed_url, clear_tables_first=True):
     else:
         log.warn("not sure what type of data we got back from {}".format(feed_url))
         ret_val = False
-    return ret_val
-
-
-def store_feed(session, agency_id, feed_type, feed, clear_tables_first):
-    ret_val = False
-    try:
-        #session.start_trans()
-        if clear_tables_first:
-            feed_type.clear_tables(session, agency_id)
-        feed_type.parse_gtfsrt_feed(session, agency_id, feed)
-        ret_val = True
-    except Exception as e:
-        log.warn(e)
-        # session.roll_back()
-    finally:
-        pass
-        # session.end_trans()
     return ret_val
 
 
@@ -76,6 +64,23 @@ def grab_feed(feed_url):
     return feed
 
 
+def store_feed(session, agency_id, feed_type, feed, clear_tables_first):
+    ret_val = False
+    try:
+        #session.start_trans()
+        if clear_tables_first:
+            feed_type.clear_tables(session, agency_id)
+        feed_type.parse_gtfsrt_feed(session, agency_id, feed)
+        ret_val = True
+    except Exception as e:
+        log.warn(e)
+        # session.roll_back()
+    finally:
+        pass
+        # session.end_trans()
+    return ret_val
+
+
 def make_session(url, schema, is_geospatial=False, create_db=False):
     """ wrap Database.make_session() ... might rethink this """
     return Database.make_session(url, schema, is_geospatial, create_db)
@@ -93,7 +98,7 @@ def main():
     aurl = string_utils.get_val(args.alerts_url, 'http://trimet.org/transweb/ws/V1/FeedSpecAlerts/includeFuture/true/appId/' + api_key)
     turl = string_utils.get_val(args.trips_url, 'http://trimet.org/transweb/ws/V1/TripUpdate/includeFuture/true/appId/' + api_key)
     vurl = string_utils.get_val(args.vehicles_url, 'http://developer.trimet.org/ws/gtfs/VehiclePositions/includeFuture/true/appId/' + api_key)
-    no_errors = load_agency_gtfsrt_data(session, args.agency, aurl, turl, vurl)
+    no_errors = load_multiple_agency_feeds(session, args.agency, aurl, turl, vurl)
     if no_errors:
         log.info("Thinking that loading went well...")
     else:
