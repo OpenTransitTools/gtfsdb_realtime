c44cff2745ae745d02ad07b95fd6706497f22eb9
Frank Purcell <fxpurcell@gmail.com>
2018-05-01 16:22:15 -0700

    add schema controls (similar to what's in gtfsdb)

--- ott/gtfsdb_realtime/model/database.py
+++ ott/gtfsdb_realtime/model/database.py
@@ -17,13 +17,12 @@ class Database(object):
 
     def __init__(self, url, schema=None, is_geospatial=False, pool_size=20):
         self.url = url
-        self.schema = schema
-        self.is_geospatial = is_geospatial
-        Base.set_schema(schema)
-        Base.set_geometry(is_geospatial)
-
         self.engine = create_engine(url, poolclass=QueuePool, pool_size=pool_size)
         event.listen(self.engine, 'connect', Database.connection)
+
+        # note ... set these after creating self.engine
+        self.schema = schema
+        self.is_geospatial = is_geospatial
 
     def create(self):
         Base.metadata.drop_all(bind=self.engine)
@@ -42,6 +41,31 @@ class Database(object):
                 cls.db_singleton.create()
         session = cls.db_singleton.get_session()
         return session
+
+    @property
+    def is_geospatial(self):
+        return self._is_geospatial
+
+    @is_geospatial.setter
+    def is_geospatial(self, val):
+        self._is_geospatial = val
+        Base.set_geometry(self._is_geospatial)
+
+    @property
+    def schema(self):
+        return self._schema
+
+    @schema.setter
+    def schema(self, val):
+        # import pdb; pdb.set_trace()
+        self._schema = val
+        try:
+            if self._schema and len(self._schema) > 0:
+                from sqlalchemy.schema import CreateSchema
+                self.engine.execute(CreateSchema(self._schema))
+                Base.set_schema(self._schema)
+        except Exception as e:
+            log.info("NOTE: couldn't create schema {0} (schema might already exist)\n{1}".format(self._schema, e))
 
     @classmethod
     def connection(cls, raw_con, connection_record):
