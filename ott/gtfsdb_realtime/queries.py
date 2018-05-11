4e7fce6e5fcfc6a596c001ce910415938aeeb2e5
Frank Purcell <fxpurcell@gmail.com>
2018-05-11 12:04:36 -0700

    rearrange print code

--- ott/gtfsdb_realtime/queries.py
+++ ott/gtfsdb_realtime/queries.py
@@ -11,12 +11,10 @@ def print_alert(alert):
     print alert.description_text
 
 def get_alerts_via_route(args):
-    print "\n\nVIA ROUTE " + args.route_id
     session = get_sessiion(args)
     from model.alert_entity import AlertEntity
     entities = AlertEntity.query_via_route_id(session, args.route_id)
-    for e in entities:
-        print_alert(e.alert)
+    return entities
 
 
 def get_alerts_via_stop(args):
@@ -24,18 +22,18 @@ def get_alerts_via_stop(args):
     session = get_sessiion(args)
     from model.alert_entity import AlertEntity
     entities = AlertEntity.query_via_stop_id(session, args.stop_id)
-    for e in entities:
-        print_alert(e.alert)
+    return entities
 
 
 def get_all_alerts(args):
-    print "\n\nALL ALERTS "
+    ret_val = []
     from model.alert import Alert
     session = get_sessiion(args)
     alerts = session.query(Alert).all()
     for a in alerts:
         for e in a.entities:
-            print_alert(e.alert)
+            ret_val.append(e)
+    return ret_val
 
 
 def get_alerts():
@@ -43,9 +41,23 @@ def get_alerts():
     gtfs_cmdline.route_option(parser)
     gtfs_cmdline.stop_option(parser)
     args = parser.parse_args()
-    if args.route_id:
-        get_alerts_via_route(args)
-    elif args.stop_id:
-        get_alerts_via_stop(args)
+
+    entities = []
+    msg = "VIA"
+    if args.route_id or args.stop_id:
+        ae = []
+        se = []
+        if args.route_id:
+            msg += " ROUTES"
+            ae = get_alerts_via_route(args)
+        if args.stop_id:
+            msg += " STOPS"
+            se = get_alerts_via_stop(args)
+        entities = ae + se
     else:
-        get_all_alerts(args)
+        msg = "ALL ALERTS"
+        entities = get_all_alerts(args)
+
+    print "\n\n", msg, ":"
+    for e in entities:
+        print_alert(e.alert)
