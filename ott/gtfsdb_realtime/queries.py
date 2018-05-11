53edd9c0d83a0c8c44b0f896e792b4e5a82cec5e
Frank Purcell <fxpurcell@gmail.com>
2018-05-11 11:41:08 -0700

    clean up the queries fun

--- ott/gtfsdb_realtime/queries.py
+++ ott/gtfsdb_realtime/queries.py
@@ -3,27 +3,49 @@ from ott.utils.parse.cmdline import gtfs_cmdline
 from model.database import Database
 
 
-def get_alerts():
+def get_sessiion(args):
+    session = Database.make_session(args.database_url, args.schema, args.is_geospatial, args.create)
+    return session
+
+def print_alert(alert):
+    print alert.description_text
+
+def get_alerts_via_route(args):
+    print "\n\nVIA ROUTE " + args.route_id
+    session = get_sessiion(args)
     from model.alert_entity import AlertEntity
-    parser = db_cmdline.db_parser('bin/gtfsrt-get-alerts')
-    gtfs_cmdline.route_option(parser)
-    args = parser.parse_args()
-
-    session = Database.make_session(args.database_url, args.schema, args.is_geospatial, args.create)
-    alerts = AlertEntity.query_via_route_id(session, args.route_id)
-    for a in alerts:
-        print a.alert.description_text
+    entities = AlertEntity.query_via_route_id(session, args.route_id)
+    for e in entities:
+        print_alert(e.alert)
 
 
-def get_all_alerts():
+def get_alerts_via_stop(args):
+    print "\n\nVIA STOP " + args.stop_id
+    session = get_sessiion(args)
+    from model.alert_entity import AlertEntity
+    entities = AlertEntity.query_via_stop_id(session, args.stop_id)
+    for e in entities:
+        print_alert(e.alert)
+
+
+def get_all_alerts(args):
+    print "\n\nALL ALERTS "
     from model.alert import Alert
-    parser = db_cmdline.db_parser('bin/gtfsrt-get-alerts')
-    gtfs_cmdline.route_option(parser)
-    args = parser.parse_args()
-
-    session = Database.make_session(args.database_url, args.schema, args.is_geospatial, args.create)
+    session = get_sessiion(args)
     alerts = session.query(Alert).all()
     for a in alerts:
         for e in a.entities:
-            print e.alert.description_text
+            print_alert(e.alert)
 
+
+def get_alerts():
+    parser = db_cmdline.db_parser('bin/gtfsrt-get-alerts')
+    gtfs_cmdline.route_option(parser)
+    gtfs_cmdline.stop_option(parser)
+    args = parser.parse_args()
+    if args.route_id:
+        get_alerts_via_route(args)
+    elif args.stop_id:
+        get_alerts_via_stop(args)
+    else:
+        get_all_alerts(args)
