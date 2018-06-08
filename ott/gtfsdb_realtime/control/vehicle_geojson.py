54872a81390fac88c72f223cf617a91b48edad82
Frank Purcell <fxpurcell@gmail.com>
2018-06-08 01:00:56 -0700

    populate json with real vehicle data

--- ott/gtfsdb_realtime/control/vehicle_geojson.py
+++ ott/gtfsdb_realtime/control/vehicle_geojson.py
@@ -1,19 +1,34 @@
 
+def set_coord(vehicle, lat, lon):
+    vehicle["properties"]['lat'] = lat
+    vehicle["properties"]['lon'] = lon
+    vehicle["geometry"]["coordinates"] = [lon, lat]
+
 
 def make_vehcile(v, i):
+    position = v.positions[0]
+
     ret_val = {
         "properties": {
-            "id": 111,
+            "~unique-id~": str(i),
             "lon": -000.111,
             "lat": 00.111,
-            "heading": "111",
+            "heading": float(position.bearing),
             "direction": 1,
+
+            # old schedule vars for map
+            "tripNumber": position.trip_id, #todo int utils safe
             "routeNumber": 1,
-            "routeNumberPadded": "001",
-            "tripNumber": 111,
-            "block": 111,
-            "vehicleNumber": "111",
-            "destination": "111 Bogus Route Name to Bogus Place via City Center",
+            "routeNumberPadded": "001", #todo int utils safe padding
+
+            "agencyId": position.agency,
+            "stopId": position.stop_id,
+            "routeId": position.route_id,
+            "tripId": position.trip_id,
+            "blockId": position.vehicle_fk,
+
+            "vehicleNumber": v.vehicle_id,
+            "destination": position.headsign,
             "minutes": 1,
             "seconds": 11,
             "reportDate": "11.11.2111 11:11 pm"
@@ -24,9 +39,7 @@ def make_vehcile(v, i):
             "coordinates": [-0.111, 0.111]
         }
     }
-
-    ret_val['id'] = i
-    #ret_val['tripNumber'] = v.get('trip')
+    set_coord(ret_val, float(position.lat), float(position.lon))
 
     return ret_val
 
@@ -43,5 +56,6 @@ def make_response(vehicles):
         v = make_vehcile(v, i)
         ret_val['features'].append(v)
         ret_val['total'] += 1
+        break
 
     return ret_val
