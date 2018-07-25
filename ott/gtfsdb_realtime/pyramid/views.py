7f11596c8573200f06e446475d5fcc3680238a78
Frank Purcell <fxpurcell@gmail.com>
2018-07-24 20:19:14 -0700

    start add for AppConfig use

--- ott/gtfsdb_realtime/pyramid/views.py
+++ ott/gtfsdb_realtime/pyramid/views.py
@@ -1,48 +1,32 @@
-from pyramid.response import Response
-from pyramid.view import view_config
-from sqlalchemy.orm.exc import NoResultFound
+from ott.utils.svr.pyramid.globals import *
 
 import logging
 log = logging.getLogger(__file__)
 
-# cache time - affects how long varnish cache will hold a copy of the data
-cache_long = 36000  # 10 hours
-cache_short = 600   # 10 minutes
 
-#system_err_msg = ServerError()
-#data_not_found = DatabaseNotFound()
-
-# database
-DB = None
-CONFIG = None
-
-
-def set_db(db):
-    global DB
-    DB = db
-
-
-def set_config(config):
-    global CONFIG
-    CONFIG = config
+APP_CONFIG=None
+def set_app_config(app_cfg):
+    """
+    called set the singleton AppConfig object
+    :see ott.utils.svr.pyramid.app_config.AppConfig :
+    """
+    global APP_CONFIG
+    APP_CONFIG = app_cfg
 
 
 def do_view_config(cfg):
     cfg.add_route('all_vehicles', '/all_vehicles')
 
 
-@view_config(route_name='all_vehicles', renderer='json', http_cache=cache_short)
+@view_config(route_name='all_vehicles', renderer='json', http_cache=CACHE_SHORT)
 def all_vehicles(request):
     ret_val = None
     try:
-        trip = get_planner().all_vehicles(request)
-        ret_val = json_response(trip)
+        ret_val = None
     except NoResultFound, e:
         log.warn(e)
-        ret_val = dao_response(data_not_found)
     except Exception as e:
         log.warn(e)
-        ret_val = dao_response(system_err_msg)
     finally:
         pass
     return ret_val
