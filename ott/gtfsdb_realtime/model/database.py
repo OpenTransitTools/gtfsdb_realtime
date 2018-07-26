e75996f4d0666377ab7789b2e3bdaf4219ab909e
Frank Purcell <fxpurcell@gmail.com>
2018-07-25 23:20:52 -0700

    change **config to ini_settings for convention sake

--- ott/gtfsdb_realtime/model/database.py
+++ ott/gtfsdb_realtime/model/database.py
@@ -18,14 +18,13 @@ class Database(object):
     """
     db_singleton = None
 
-    def __init__(self, url, schema=None, is_geospatial=False, pool_size=20):
+    def __init__(self, url, schema=None, is_geospatial=False, pool_size=20, session_extenstion=None):
         self.url = url
         self.engine = create_engine(url, poolclass=QueuePool, pool_size=pool_size)
         event.listen(self.engine, 'connect', Database.connection)
 
         # note ... set these after creating self.engine
-        self.session_factory = sessionmaker(bind=self.engine)
-        self.Session = scoped_session(self.session_factory)
+        self._make_session_class(session_extenstion)
         self.schema = schema
         self.is_geospatial = is_geospatial
 
@@ -37,6 +36,15 @@ class Database(object):
     def session(self):
         session = self.Session()
         return session
+
+    def _make_session_class(self, extension=None):
+        """
+        this makes the Session() class ... the extension is for things like
+        :see http://docs.sqlalchemy.org/en/latest/orm/contextual.html?highlight=scoped%20session :
+        :see https://www.programcreek.com/python/example/97518/zope.sqlalchemy.ZopeTransactionExtension :
+        """
+        self.session_factory = sessionmaker(bind=self.engine, extension=extension)
+        self.Session = scoped_session(self.session_factory)
 
     @classmethod
     def make_session(cls, url, schema, is_geospatial=False, create_db=False):
--- ott/gtfsdb_realtime/pyramid/app.py
+++ ott/gtfsdb_realtime/pyramid/app.py
@@ -4,20 +4,20 @@ import logging
 log = logging.getLogger(__file__)
 
 
-def main(global_config, **config):
+def main(global_config, **ini_settings):
     """
     this function is the main entry point for pserve / Pyramid
     it returns a Pyramid WSGI application
     see setup.py entry points + config/*.ini [app:main] ala pserve (e.g., bin/pserve config/development.ini)
     """
-    app = AppConfig(**config)
+    app = AppConfig(**ini_settings)
 
     from ott.gtfsdb_realtime.model.database import Database
+    from zope.sqlalchemy import ZopeTransactionExtension
     u, s, g = app.db_params_from_config()
-    db = Database(u, s, g)
+    db = Database(u, s, g, session_extenstion=ZopeTransactionExtension())
     app.set_db(db)
 
     import views
     app.config_include_scan(views)
     return app.make_wsgi_app()
-
