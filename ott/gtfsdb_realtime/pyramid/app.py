b470f8c208adc0a2243e9bf24af7d36279223200
Frank Purcell <fxpurcell@gmail.com>
2019-02-15 09:53:34 -0800

    refactor CORS header code

--- ott/gtfsdb_realtime/pyramid/app.py
+++ ott/gtfsdb_realtime/pyramid/app.py
@@ -4,30 +4,6 @@ import logging
 log = logging.getLogger(__file__)
 
 
-def add_cors_headers_response_callback(event):
-    MAX_AGE = '1728000'
-
-    """
-    add CORS so the requests can work from different (test / development) port
-    do this at least for testing ... might not make call in production
-    :param event:
-
-    :see config.add_subscriber(add_cors_headers_response_callback, NewRequest):
-
-    :see credit goes to https://stackoverflow.com/users/211490/wichert-akkerman
-    :see https://stackoverflow.com/questions/21107057/pyramid-cors-for-ajax-requests
-    """
-    def cors_headers(request, response):
-        response.headers.update({
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
-            'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
-            'Access-Control-Allow-Credentials': 'true',
-            'Access-Control-Max-Age': MAX_AGE,
-        })
-    event.request.add_response_callback(cors_headers)
-
-
 def main(global_config, **ini_settings):
     """
     this function is the main entry point for pserve / Pyramid
@@ -36,7 +12,7 @@ def main(global_config, **ini_settings):
     """
     app = AppConfig(**ini_settings)
 
-    from pyramid.events import NewRequest
+
     from zope.sqlalchemy import ZopeTransactionExtension
     from ott.gtfsdb_realtime.model.database import Database
     import views
@@ -45,8 +21,7 @@ def main(global_config, **ini_settings):
     db = Database(u, s, g, session_extenstion=ZopeTransactionExtension())
     app.set_db(db)
 
-    app.pyramid.add_subscriber(add_cors_headers_response_callback, NewRequest)
-
+    app.add_cors_headers()
     app.config_include_scan(views)
     return app.make_wsgi_app()
 
